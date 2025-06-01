974d581e92df3acc1b625a1579877aced836fa9b
HishamElhamayel <elhamayelh@gmail.com>
2025-06-01 09:32:03 +0300

    added callback after signin

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -4,12 +4,20 @@ import sampleData from "./sample-data";
 const seed = async () => {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({
     data: sampleData.products,
   });
 
-  console.log("Database seeded successfully");
+  await prisma.user.createMany({
+    data: sampleData.users,
+  });
+
+  await console.log("Database seeded successfully");
 };
 
 seed();
